9b0aacedd6022ecc029aa2c620ca7a120022446a
rodbundy <sr.rodbundy@gmail.com>
2026-07-12 09:44:25 -0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.STORM_CONFIG = {
   // Paste the deployed Google Apps Script Web App URL below. It ends in /exec.
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwg1R0dEQhYar2-Gco0PLQFE0Zq0o_5J3NTkYJYa0FtSGDAc1_5_g4TiqSrPn4PAXlviA/exec",
   teamName: "2 Out Storm 10U",
   domain: "2outstorm2035.com",
   fallbackPhone: "+18594144622",
